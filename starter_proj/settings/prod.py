bdd68204ecf3918eb8009ace3a69d4e492d4ccac
great-special <sirgreatspecial@gmail.com>
2025-03-27 15:16:12 +0100

    ready for production

--- starter_proj/settings/prod.py
+++ starter_proj/settings/prod.py
@@ -1,4 +1,7 @@
-from .base import * 
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-rb=i^unt@gd4sbptx_xab6aa04x=5i_1$w@ylm!q@&zrt$n+-)'
@@ -7,6 +10,103 @@ SECRET_KEY = 'django-insecure-rb=i^unt@gd4sbptx_xab6aa04x=5i_1$w@ylm!q@&zrt$n+-)
 DEBUG = False
 
 ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    "tailwind",
+    "theme",  # Name of your Tailwind app
+    "django_browser_reload",  # Optional: Enables live reloading
+    
+    'core.apps.CoreConfig',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
+]
+
+ROOT_URLCONF = 'starter_proj.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR/'templates'],  # Add this line
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'starter_proj.wsgi.application'
+
+# Password validation
+# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+# Tailwind CSS Configuration
+TAILWIND_APP_NAME = 'theme'  # Name of your Tailwind app
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+from shutil import which
+NPM_BIN_PATH = which("npm")
+
+# NPM_BIN_PATH = 'C:\Users\Great Speed\AppData\Roaming\fnm\node-versions\v22.13.1\installation\npm.CMD'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
